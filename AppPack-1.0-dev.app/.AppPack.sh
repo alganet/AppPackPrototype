06fe2789e6e86d544f3eeb3feb8c8bcb55c915a6
Alexandre Gaigalas <alexandre@gaigalas.net>
2015-05-18 00:18:08 -0300

    Icon refresh for desktop entries with backup

--- AppPack-1.0-dev.app/.AppPack.sh
+++ AppPack-1.0-dev.app/.AppPack.sh
@@ -2,6 +2,11 @@
 
 cd "$(dirname "$0")"
 
+IniFile="$(find -name *.desktop)"
+
+cp "${IniFile}" "${IniFile}.bak"
+sed -e "s,Icon=.*,Icon=${PWD}/Contents/Resources/AppPack.ico,g" "${IniFile}.bak" > "${IniFile}"
+
 . "./Contents/Portable/AppPack-1.0/AppPack.sh"
 
 AppPack "${@:-}"
