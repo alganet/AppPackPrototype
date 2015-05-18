463b66d78bf4ca92fa7ea732894c571af745bee3
Alexandre Gaigalas <alexandre@gaigalas.net>
2015-05-17 22:58:14 -0300

    Windows proper start command for starting browsers

--- AppPack-1.0-dev.app/.AppPack.bat
+++ AppPack-1.0-dev.app/.AppPack.bat
@@ -3,17 +3,17 @@
 set AppHtml=./Contents/Portable/AppPack-1.0/AppPack.html
 
 if exist "%ProgramFiles%\Mozilla Firefox\firefox.exe" (
-	"%ProgramFiles%\Mozilla Firefox\firefox.exe" -chrome %AppHtml%
+	start "" "%ProgramFiles%\Mozilla Firefox\firefox.exe" -chrome %AppHtml%
 ) else if exist "%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe" (
-	"%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe" -chrome %AppHtml%
+	start "" "%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe" -chrome %AppHtml%
 ) else if exist "%ProgramFiles%\Firefox Developer Edition\firefox.exe" (
-	"%ProgramFiles%\Firefox Developer Edition\firefox.exe" -chrome %AppHtml%
+	start "" "%ProgramFiles%\Firefox Developer Edition\firefox.exe" -chrome %AppHtml%
 ) else if exist "%ProgramFiles(x86)%\Firefox Developer Edition\firefox.exe" (
-	"%ProgramFiles(x86)%\Firefox Developer Edition\firefox.exe" -chrome %AppHtml%
+	start "" "%ProgramFiles(x86)%\Firefox Developer Edition\firefox.exe" -chrome %AppHtml%
 ) else if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" (
-	"%ProgramFiles%\Google\Chrome\Application\chrome.exe" --app=%__CD__%%AppHtml%
+	start "" "%ProgramFiles%\Google\Chrome\Application\chrome.exe" --app=%__CD__%%AppHtml%
 ) else if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" (
-	"%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" --app=%__CD__%%AppHtml%
+	start "" "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" --app=%__CD__%%AppHtml%
 ) else (
 	start "" ".\Contents\Windows\AppPack.hta"
 )
