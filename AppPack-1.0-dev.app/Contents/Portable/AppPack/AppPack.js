1c6a1ba4c4a5f6c2b04639004faf453c70eb18c0
Alexandre Gaigalas <alexandre@gaigalas.net>
2016-05-25 13:07:03 -0300

    proto

--- AppPack-1.0-dev.app/Contents/Portable/AppPack/AppPack.js
+++ AppPack-1.0-dev.app/Contents/Portable/AppPack/AppPack.js
@@ -1,69 +1,59 @@
 (function (root, target) {
 
-	target(root);
+    target(root);
 
 })(this, function () {
 
-	"use strict";
+    "use strict";
 
-	this.AppPack = function () {};
-
-	this.AppPack.prototype.show = function () {
-	    var FileObject = new ActiveXObject('Scripting.FileSystemObject'),
-	        IniStream  = FileObject.OpenTextFile(".AppPack.ini", 1, false);
-
-	    while( ! IniStream.AtEndOfStream) {
-	        document.body.innerHTML += IniStream.ReadLine() + '<br/>';
-	    }
-
-	    IniStream.Close();
-	}
+    this.AppPack = function () {};
 
     this.AppPack.prototype.boot = function () {
 
-	    function RemoveZoneIdentifier(f, path, fs) {
-	        var SubFolders = new Enumerator(f.SubFolders);
-	        var SubFiles   = new Enumerator(f.files);
+        function RemoveZoneIdentifier(f, path, fs) {
+            var SubFolders = new Enumerator(f.SubFolders);
+            var SubFiles   = new Enumerator(f.files);
 
-	        for (; !SubFolders.atEnd(); SubFolders.moveNext()) {
-	            RemoveZoneIdentifier(
-	                SubFolders.item(),
-	                fs.BuildPath(path, SubFolders.item().Name),
-	                fs
-	            );
-	        }
+            for (; !SubFolders.atEnd(); SubFolders.moveNext()) {
+                RemoveZoneIdentifier(
+                    SubFolders.item(),
+                    fs.BuildPath(path, SubFolders.item().Name),
+                    fs
+                );
+            }
 
-	        for (; !SubFiles.atEnd(); SubFiles.moveNext()) {
-	            try {
-	                var FileStream = fs.OpenTextFile(
-	                    path + "\\" + SubFiles.item().Name+':Zone.Identifier',
-	                    2
-	                );
-	                FileStream.Close();
-	            } catch(e) {
+            for (; !SubFiles.atEnd(); SubFiles.moveNext()) {
+                try {
+                    var FileStream = fs.OpenTextFile(
+                        path + "\\" + SubFiles.item().Name+':Zone.Identifier',
+                        2
+                    );
+                    FileStream.Close();
+                } catch(e) {
 
-	            }
-	        }
-	    }
+                }
+            }
+        }
 
-	    var FileSystem        = new ActiveXObject("Scripting.FileSystemObject");
-	    var Shell             = new ActiveXObject("WScript.Shell");
-	    var Shortcut          = Shell.CreateShortcut(".\\AppPack-1.0-dev.lnk");
-	    Shortcut.IconLocation = Shell.currentDirectory + "\\Contents\\Resources\\AppPack.ico";
+        var FileSystem        = new ActiveXObject("Scripting.FileSystemObject");
+        var Shell             = new ActiveXObject("WScript.Shell");
+        var Shortcut          = Shell.CreateShortcut(".\\AppPack-1.0-dev.lnk");
+        Shortcut.IconLocation = Shell.currentDirectory + "\\Contents\\Resources\\AppPack.ico";
 
-	    Shortcut.Save();
+        Shortcut.Save();
 
-	    RemoveZoneIdentifier(
-	        FileSystem.GetFolder('.'),
-	        FileSystem.GetAbsolutePathName('.'),
-	        FileSystem
-	    );
+        RemoveZoneIdentifier(
+            FileSystem.GetFolder('.'),
+            FileSystem.GetAbsolutePathName('.'),
+            FileSystem
+        );
 
-	    Shell.Run(
-	    	Shell.currentDirectory +
-	    	"\\Contents\\Windows\\AppPack.hta " +
-	    	Shell.currentDirectory +
-	    	"\\Contents\\Portable\\AppPack\\AppPack.html"
-	    );
+        Shell.Run(
+            "mshta " +
+            Shell.currentDirectory +
+            "\\Contents\\Windows\\AppPack.hta " +
+            Shell.currentDirectory +
+            "\\Contents\\Portable\\AppPack\\AppPack.html"
+        );
     };
 });
